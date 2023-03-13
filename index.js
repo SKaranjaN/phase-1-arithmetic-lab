0fd7b3a989c5f4879c1d16fd9e33096bd7bba253
Stephen <skaranja654@gmail.com>
2023-03-13 22:07:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+let random = 1;
+Number.isInteger(random);
+
+let num3 = 18;
+let num4 = 7;
+let mod = (num3 % num4);
+
+let max = (20);
+max.max(20);
+
+
